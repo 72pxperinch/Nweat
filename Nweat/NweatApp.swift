c68f6ea4c7fc25aebbd2553772198bfc50b94f06
72pxperich <72pxperinch@gmail.com>
2024-11-26 18:59:13 +0530

    Initial Commit

---
+++ Nweat/NweatApp.swift
@@ -0,0 +1,17 @@
+//
+//  NweatApp.swift
+//  Nweat
+//
+//  Created by Hafidh Farhan S on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NweatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NweatTests/NweatTests.swift
@@ -0,0 +1,17 @@
+//
+//  NweatTests.swift
+//  NweatTests
+//
+//  Created by Hafidh Farhan S on 26/11/24.
+//
+
+import Testing
+@testable import Nweat
+
+struct NweatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
